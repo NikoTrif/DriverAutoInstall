03e4f469720b5c2093fe5cb401dc125e488ff1a4
NikoTrif <60791847+NikoTrif@users.noreply.github.com>
2021-04-30 16:52:56 +0200

    fDAI; ucSetInstall - Start

---
+++ DriverAutoInstall/fDAI.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace DriverAutoInstall
+{
+    partial class fDAI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pMain = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pMain
+            // 
+            this.pMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pMain.Location = new System.Drawing.Point(0, 0);
+            this.pMain.Name = "pMain";
+            this.pMain.Size = new System.Drawing.Size(577, 332);
+            this.pMain.TabIndex = 0;
+            // 
+            // fDAI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 332);
+            this.Controls.Add(this.pMain);
+            this.Name = "fDAI";
+            this.Text = "Driver AutoInstall";
+            this.Load += new System.EventHandler(this.fDAI_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pMain;
+    }
+}
+
---
+++ DriverAutoInstall/ucSetInstall.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace DriverAutoInstall
+{
+    partial class ucSetInstall
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dDown = new System.Windows.Forms.Button();
+            this.dUp = new System.Windows.Forms.Button();
+            this.dExit = new System.Windows.Forms.Button();
+            this.dImpXml = new System.Windows.Forms.Button();
+            this.dSaveXml = new System.Windows.Forms.Button();
+            this.dAdd = new System.Windows.Forms.Button();
+            this.dInstall = new System.Windows.Forms.Button();
+            this.flpDriveri = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmsNaziv = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.flpDriveri.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dDown
+            // 
+            this.dDown.Location = new System.Drawing.Point(527, 135);
+            this.dDown.Name = "dDown";
+            this.dDown.Size = new System.Drawing.Size(29, 26);
+            this.dDown.TabIndex = 3;
+            this.dDown.Text = "D";
+            this.dDown.UseVisualStyleBackColor = true;
+            // 
+            // dUp
+            // 
+            this.dUp.Location = new System.Drawing.Point(527, 103);
+            this.dUp.Name = "dUp";
+            this.dUp.Size = new System.Drawing.Size(29, 26);
+            this.dUp.TabIndex = 4;
+            this.dUp.Text = "U";
+            this.dUp.UseVisualStyleBackColor = true;
+            // 
+            // dExit
+            // 
+            this.dExit.Location = new System.Drawing.Point(481, 288);
+            this.dExit.Name = "dExit";
+            this.dExit.Size = new System.Drawing.Size(75, 23);
+            this.dExit.TabIndex = 5;
+            this.dExit.Text = "Exit";
+            this.dExit.UseVisualStyleBackColor = true;
+            // 
+            // dImpXml
+            // 
+            this.dImpXml.Location = new System.Drawing.Point(246, 288);
+            this.dImpXml.Name = "dImpXml";
+            this.dImpXml.Size = new System.Drawing.Size(75, 23);
+            this.dImpXml.TabIndex = 6;
+            this.dImpXml.Text = "Import XML";
+            this.dImpXml.UseVisualStyleBackColor = true;
+            // 
+            // dSaveXml
+            // 
+            this.dSaveXml.Location = new System.Drawing.Point(165, 288);
+            this.dSaveXml.Name = "dSaveXml";
+            this.dSaveXml.Size = new System.Drawing.Size(75, 23);
+            this.dSaveXml.TabIndex = 7;
+            this.dSaveXml.Text = "Save XML";
+            this.dSaveXml.UseVisualStyleBackColor = true;
+            // 
+            // dAdd
+            // 
+            this.dAdd.Location = new System.Drawing.Point(84, 288);
+            this.dAdd.Name = "dAdd";
+            this.dAdd.Size = new System.Drawing.Size(75, 23);
+            this.dAdd.TabIndex = 8;
+            this.dAdd.Text = "Add";
+            this.dAdd.UseVisualStyleBackColor = true;
+            // 
+            // dInstall
+            // 
+            this.dInstall.Location = new System.Drawing.Point(3, 288);
+            this.dInstall.Name = "dInstall";
+            this.dInstall.Size = new System.Drawing.Size(75, 23);
+            this.dInstall.TabIndex = 9;
+            this.dInstall.Text = "Install";
+            this.dInstall.UseVisualStyleBackColor = true;
+            // 
+            // flpDriveri
+            // 
+            this.flpDriveri.Controls.Add(this.tableLayoutPanel1);
+            this.flpDriveri.Location = new System.Drawing.Point(3, 3);
+            this.flpDriveri.Name = "flpDriveri";
+            this.flpDriveri.Size = new System.Drawing.Size(518, 277);
+            this.flpDriveri.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 69F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 355F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(493, 92);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Naziv:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Putanja:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Parametri:";
+            // 
+            // cmsNaziv
+            // 
+            this.cmsNaziv.Name = "cmsNaziv";
+            this.cmsNaziv.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(74, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(349, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(74, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(349, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(74, 66);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(349, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(463, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(430, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(26, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(430, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(26, 24);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "?";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // ucSetInstall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dDown);
+            this.Controls.Add(this.dUp);
+            this.Controls.Add(this.dExit);
+            this.Controls.Add(this.dImpXml);
+            this.Controls.Add(this.dSaveXml);
+            this.Controls.Add(this.dAdd);
+            this.Controls.Add(this.dInstall);
+            this.Controls.Add(this.flpDriveri);
+            this.Name = "ucSetInstall";
+            this.Size = new System.Drawing.Size(577, 332);
+            this.flpDriveri.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button dDown;
+        private System.Windows.Forms.Button dUp;
+        private System.Windows.Forms.Button dExit;
+        private System.Windows.Forms.Button dImpXml;
+        private System.Windows.Forms.Button dSaveXml;
+        private System.Windows.Forms.Button dAdd;
+        private System.Windows.Forms.Button dInstall;
+        private System.Windows.Forms.FlowLayoutPanel flpDriveri;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ContextMenuStrip cmsNaziv;
+    }
+}
